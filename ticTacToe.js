883be5c7ec74912f85ba2c4e5655bc2a96864184
David Seidenberg <davidseidenberg@gmail.com>
2018-06-14 16:34:58 -0700

    Basic gameplay achieved

--- ticTacToe.js
+++ ticTacToe.js
@@ -2,41 +2,69 @@
 
 const readline = require('readline');
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const makeBoard = () => {
+  const board = new Array(3);
+  for (let i = 0; i < board.length; i++) {
+    board[i] = new Array(3);
+  }
+  return board;
+}
+
 class Game {
   constructor() {
-    this.board = new Array(3);
-    for (let i = 0; i < this.board.length; i++) {
-      this.board[i] = new Array(3);
-    }
-
-    this.currentPlayer = 'X';
-    this.gameState = 'play';
+    this.board = makeBoard();
+    this.player = 'X';
+    this.state = 'play';
   }
 }
 
 let myGame = new Game;
 
-// make display board function
+const printBoard = (board) => {
+  console.log('\n')
+  printRow(board[0]);
+  console.log('_________');
+  printRow(board[1]);
+  console.log('_________');
+  printRow(board[2]);
+  console.log('\n')
 
-const printBoard = (game) => {
-  printRow(game.board[0]);
-  console.log('_________');
-  printRow(game.board[1]);
-  console.log('_________');
-  printRow(game.board[2]);
 };
 
 const printRow = (row) => {
   let c0 = row[0] || ' ';
   let c1 = row[1] || ' ';
   let c2 = row[2] || ' ';
-
   console.log(`${c0} | ${c1} | ${c2} `);
 }
-// prompt user for input
+
+const updateBoard = (board, input, player) => {
+  let row = input[0];
+  let col = input[1];
+  board[row][col] = player;
+}
+
+const getMove = (updateBoard, board, player) => {
+    rl.question(`Player ${player}, please input the row and column for your next move: `, (answer) => {
+      // update board
+      updateBoard(board, answer, player);
+      printBoard(board);
+      // if board is full return draw
+      // if winner return win
+      // else 
+      player === 'X' ? player = 'O' : player = 'X';
+      getMove(updateBoard, board, player);
+  });
+}
 
 // calculate win scenarios
 
 // end game
 
-printBoard(myGame);
+printBoard(myGame.board);
+getMove(updateBoard, myGame.board, myGame.player);
